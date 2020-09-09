3f5e1da9cb29fd75a5bd8e429af7a093e59327a0
rnajath <Rushdy.Najath@experian.com>
2020-09-09 12:26:15 +0100

    clean up

--- BMS.Libraray.BusinessLayer/Models/BooksCategory.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BMS.Library.BusinessLayer.Models
-{
-    public class BooksCategory
-    {
-        public string CategoryId { get; set; }
-        public string Name { get; set; }
-        public string Comment { get; set; }
-    }
-}
